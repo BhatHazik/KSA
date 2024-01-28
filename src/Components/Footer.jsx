dcccbc1e989c52931de278b57362e0e6166ee931
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-01-28 17:58:23 +0530

    v1.4

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../styles/Footer.css"
 import { Link } from 'react-router-dom'
+import img3 from "../Assets/img3.png"
 const Footer = () => {
   return (
     <div className='footer'>
@@ -15,9 +16,18 @@ const Footer = () => {
       <Link>Top Rankers</Link>
       </div>
       <div className='footer-contact'>
+        <img src={img3}/>
+        <div>
+          <h5>If You'r Having Any Issue Just Mail Us On</h5>
+          <h4>kisaracademics@gmail.com</h4>
+        </div>
         
+
       </div>
-      <div className='footer-devloper'></div>
+      <div className='footer-developer'>
+        <h2>Developer And Founder Of This App</h2>
+        <h3>Proudly Crafted In Kashmir, With Passion By HAZIK HILAL</h3>
+      </div>
       </div>
       
     </div>
